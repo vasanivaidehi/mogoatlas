905698e7b8964234924ec3be81dfbb125e38dbb5
vasanivaidehi <vasanivaidehi42@gmail.com>
2026-03-05 15:35:31 +0530

    some changes in the file

--- insert.py
+++ insert.py
@@ -5,12 +5,16 @@ client = MongoClient("mongodb+srv://vasanivaidehi42_db_user:pymongo@pymongo.xaiv
 db = client["testdb"]
 collection = db["users"]
 
-data =[ {"name": "Vaidehi","age": 21,"city": "Rajkot"},
-        {"name": "Krishna","age": 20,"city": "Rajkot"},
-        {"name": "Aarti","age": 22,"city": "Rajkot"}
-]
+#data =[ {"name": "Grishma","age": 21,"city": "Rajkot"},
+#        {"name": "Palak","age": 20,"city": "Rajkot"},
+#        {"name": "Disha","age": 22,"city": "Rajkot"}
+#]
 
 #collection.insert_one(data)
-collection.insert_many(data)
-
-print("Multiple Data Inserted Successfully ")
+#collection.insert_many(data)
+prev={"name":"vishva"}
+nextt={"$set":{"city":"mumbai"}}
+    #collection.update_one(prev,nextt)
+up=collection.update_one(prev,nextt)
+print(up.modified_count)
+print("Data Updated Successfully ")
