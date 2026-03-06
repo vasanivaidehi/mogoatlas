44cdc2bdf2027c143da9b3c734fe999630e869ba
vasanivaidehi <vasanivaidehi42@gmail.com>
2026-03-06 16:38:06 +0530

    Update insert.py

--- insert.py
+++ insert.py
@@ -12,7 +12,7 @@ data =[ {"name": "Aarti","city": "Rajkot","email":"G@gmail.com"},
 
 #collection.insert_one(data)
 collection.insert_many(data)
-prev={"name":"Aarti"}
+#prev={"name":"Aarti"}
 #nextt={"$set":{"city":"surat"}}
     #collection.update_one(prev,nextt)
 #up=collection.update_one(prev,nextt)
